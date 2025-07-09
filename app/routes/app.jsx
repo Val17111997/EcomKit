d8d5277deafcc7591e313a1c024021af897b6b45
Valentin Cluzeau <valentincluzeau@MacBook-Air-de-Valentin.local>
2025-07-09 19:11:05 +0200

    test 3

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,10 +1,8 @@
-import { redirect } from "@remix-run/node";
 import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
 import { boundary } from "@shopify/shopify-app-remix/server";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
 import { NavMenu } from "@shopify/app-bridge-react";
 import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
-import { authenticate } from "../shopify.server";
 
 export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
@@ -12,7 +10,9 @@ export const loader = async ({ request }) => {
   // Replace with the "app_handle" from your shopify.app.toml file
   const appHandle = "ecom-kit-2";
   // Authenticate with Shopify credentials to handle server-side queries
-  const { billing, session } = await authenticate.admin(request);
+  const { authenticate } = await import("../shopify.server");
+  // Initiate billing and redirect utilities
+  const { billing, redirect, session } = await authenticate.admin(request);
   // Check whether the store has an active subscription
   const { hasActivePayment } = await billing.check();
   // Extract the store handle from the shop domain
